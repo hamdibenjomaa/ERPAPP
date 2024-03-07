e2ce9bf28dea92faa2ed4321660a10eabba8a1f2
MalekZaidi <melekzaidi407@gmail.com>
2024-03-07 17:21:54 +0100

    User Session Sec

--- src/app/front-office/front-office.module.ts
+++ src/app/front-office/front-office.module.ts
@@ -6,15 +6,10 @@ import { SignupComponent } from './components/signin/signin.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarFrontComponent } from './components/navbar-front/navbar-front.component';
 import { FooterFrontComponent } from './components/footer-front/footer-front.component';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 import { PasswordResetComponent } from './components/password-reset/password-reset.component';
 import { ForgotPasswordComponent } from './components/forget-password/forget-password.component';
 import { ToastrModule } from 'ngx-toastr';
-=======
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> 9802b1ba3fc1f21eea1f47fea931fe2a533517ac
 
 
 @NgModule({
@@ -31,15 +26,12 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     FrontOfficeRoutingModule,
-<<<<<<< HEAD
     FormsModule,
     ToastrModule.forRoot() // Add ToastrModule.forRoot() here
   ],
     
   
-=======
     
-  ]
->>>>>>> 9802b1ba3fc1f21eea1f47fea931fe2a533517ac
+
 })
 export class FrontOfficeModule { }
